2ffc1e7e4673ba93c3da9cacdab9f108c2e399f3
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-23 12:44:41 +0545

    setuped projects

---
+++ src/store/reducers/ColorsReducer.js
@@ -0,0 +1,12 @@
+const initalState = {
+  primary: '',
+  navText: '',
+  text: '',
+  navHover: '',
+  pageBackground: '',
+  screenBackground: '',
+};
+
+export default function colorReducer(state = initalState, actions) {
+  return state;
+}
---
+++ src/templetes/MainTempletes.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function MainTempletes() {
+  return <div>templtes</div>;
+}
