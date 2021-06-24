faffdfc9333481beee0669d2ec70c9a115e3ada9
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-24 15:31:47 +0545

    added admin data

--- src/store/reducers/AdminReducer.js
+++ src/store/reducers/AdminReducer.js
@@ -2,6 +2,21 @@ const initailState = {
   name: 'Bibek Shrestha',
   title: 'MERN Stack Developer',
   isAdmin: false,
+  about: {
+    title: "Hellow, I'm Bibek",
+  },
+  education: {
+    title: 'Educations',
+  },
+  project: {
+    title: 'Projects',
+  },
+  skill: {
+    title: 'Programming Language & Tools',
+  },
+  contact: {
+    title: "Let's Chat!",
+  },
 };
 
 export default function mainReducer(state = initailState, actions) {
