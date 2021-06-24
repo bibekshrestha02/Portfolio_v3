ed39141f5f24f83aa85a7525e8035e66a67cfe76
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-24 11:41:39 +0545

    created main page templetes

--- src/templetes/MainTempletes.jsx
+++ src/templetes/MainTempletes.jsx
@@ -1,4 +1,21 @@
 import React from 'react';
-export default function MainTempletes() {
-  return <div>templtes</div>;
+import { useSelector } from 'react-redux';
+import NavComponent from '../components/Nav/NavComponent';
+import style from './style.module.scss';
+export default function MainTempletes({ children }) {
+  const { pageBackground, primary } = useSelector((state) => state.colors);
+  const container = {
+    backgroundColor: pageBackground,
+  };
+  const bodyContainer = {
+    border: `2px solid ${primary}`,
+  };
+  return (
+    <div className={style.mainTempleteContainer} style={container}>
+      <div className={style.bodyContainer} style={bodyContainer}>
+        <NavComponent />
+        {children}
+      </div>
+    </div>
+  );
 }
