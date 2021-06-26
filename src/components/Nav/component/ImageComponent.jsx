08886ab2ce58d26cb31888b99573fbe0e6f9a778
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-26 15:02:59 +0545

    implemented profile image loading

--- src/components/Nav/component/ImageComponent.jsx
+++ src/components/Nav/component/ImageComponent.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '../style.module.scss';
 import { IoMdColorPalette } from 'react-icons/io';
+import { BsFillPersonFill } from 'react-icons/bs';
+
 export default function ImageComponent({ image, isAdmin, colors }) {
+  const [isImageLoading, setImageLoading] = useState(true);
   return (
     <div className={style.imageContainer}>
+      {isImageLoading && (
+        <div
+          className={style.image}
+          style={{
+            display: isImageLoading ? 'block' : 'none',
+          }}>
+          <BsFillPersonFill color={colors.text} />
+        </div>
+      )}
       <img
+        onLoad={() => setImageLoading(false)}
         src={image}
         alt='profile'
         className={style.image}
-        style={{ border: `5px solid ${colors.navText}` }}
+        style={{
+          border: `5px solid ${colors.navText}`,
+          display: isImageLoading ? 'none' : 'block',
+        }}
       />
       {isAdmin && (
         <span style={{ color: colors.navText }}>Change Profile Path</span>
