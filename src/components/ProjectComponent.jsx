8a268df5b987b87f9913c2ecc8bb1e11cf1c3c85
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-26 15:19:10 +0545

    impletement imagloading component

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './style.module.scss';
+import ImageLoadingComponent from './ImageLoadingComponent';
 export default function ProjectComponent({ name, path, icon }) {
+  const [isImageLoading, setImageLoading] = useState(true);
   return (
     <div className={style.projectComponent}>
       <a href={path} target='_blank' rel='noreferrer'>
-        <img src={icon} alt='project' />
+        <ImageLoadingComponent isLoading={isImageLoading} />
+        <img
+          src={icon}
+          alt='project'
+          style={{ display: isImageLoading ? 'none' : 'block' }}
+          onLoad={() => setImageLoading(false)}
+          onError={() => setImageLoading(true)}
+        />
         <span>{name}</span>
       </a>
     </div>
