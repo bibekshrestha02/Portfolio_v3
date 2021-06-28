35ea4eaa785e03eefa2b18af3710a2f5e9ea53e4
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-28 11:24:45 +0545

    added styles props

--- src/assets/ButtonAssets.jsx
+++ src/assets/ButtonAssets.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import style from './style.module.scss';
 import { useSelector } from 'react-redux';
-export default function ButtonAssets({ props, title }) {
+export default function ButtonAssets({ props, title, styles }) {
   const { primary } = useSelector((state) => state.colors);
   return (
     <button
-      style={{ backgroundColor: primary }}
+      style={{ backgroundColor: primary, ...styles }}
       className={style.button}
       {...props}>
       {title}
