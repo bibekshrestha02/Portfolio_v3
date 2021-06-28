44b0c6a9cc1a14cd875e08a9834837de36972804
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-28 10:46:07 +0545

    added edit buttom

--- src/components/Nav/component/SocialLinksComponent.jsx
+++ src/components/Nav/component/SocialLinksComponent.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import style from '../style.module.scss';
-import { IoIosAddCircleOutline, IoMdRemove } from 'react-icons/io';
+import { IoAddCircleOutline, IoRemoveOutline } from 'react-icons/io5';
+
+import { AiOutlineEdit } from 'react-icons/ai';
+
 export default function SocialLinksComponent({ socialLinks, isAdmin, colors }) {
   return (
     <div className={style.socialLinksContainer}>
       {socialLinks.map((link) => {
         return (
           <div className={style.iconContainer} key={link.icon}>
+            {isAdmin && (
+              <div className={style.removeContainer}>
+                <IoRemoveOutline color={colors.warning} size={18} />
+                <AiOutlineEdit color={colors.navText} size={18} />
+              </div>
+            )}
             <a href={link.link} target='_blank' rel='noreferrer'>
               <img src={link.icon} alt='icon' className={style.icon} />
             </a>
-            {isAdmin && (
-              <div className={style.removeContainer}>
-                <IoMdRemove color={colors.warning} />
-              </div>
-            )}
           </div>
         );
       })}
+
       {isAdmin && (
         <div className={style.iconContainer}>
-          <IoIosAddCircleOutline color={colors.navText} size={30} />
+          <IoAddCircleOutline color={colors.navText} size={30} />
         </div>
       )}
     </div>
