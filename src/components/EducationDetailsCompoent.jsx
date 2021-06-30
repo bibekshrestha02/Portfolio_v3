8f4b74cda0855b084612e6c2c7e2c0da43db8992
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-30 12:50:54 +0545

    added update and delete features

--- src/components/EducationDetailsCompoent.jsx
+++ src/components/EducationDetailsCompoent.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import style from './style.module.scss';
 import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
+import MyTextInput from './MyInputsCompoenent/MyTextInput';
+const ModelComponent = lazy(() => import('../components/ModelComponent'));
 export default function EducationDetailsCompoent({
   name,
   place,
@@ -8,32 +10,85 @@ export default function EducationDetailsCompoent({
   branch,
   isAdmin,
   colors,
+  validationSchema,
+  updateHandler,
+  deleteHandler,
 }) {
+  const [isUpdateModel, setUpdateModel] = React.useState(false);
+  const updateModelHandler = () => {
+    return setUpdateModel((e) => !e);
+  };
+  const educationInitalValue = {
+    name,
+    place,
+    year,
+    branch,
+  };
   return (
-    <div className={style.educationContainer}>
-      <div>
-        <span className={style.title}>{name}</span>
-        <p>{place}</p>
-        <span>{branch}</span>
+    <>
+      <div className={style.educationContainer}>
+        <div>
+          <span className={style.title}>{name}</span>
+          <p>{place}</p>
+          <span>{branch}</span>
+        </div>
+        <div>
+          {isAdmin && (
+            <div>
+              <AiOutlineDelete
+                color={colors.warning}
+                size={20}
+                onClick={() => {
+                  deleteHandler(name);
+                }}
+                className={style.icon}
+              />
+              <AiOutlineEdit
+                size={20}
+                onClick={updateModelHandler}
+                className={style.icon}
+                color={colors.primary}
+              />
+            </div>
+          )}
+          <span>{year}</span>
+        </div>
       </div>
-      <div>
-        {isAdmin && (
-          <div>
-            <AiOutlineDelete
-              color={colors.warning}
-              size={20}
-              className={style.icon}
-            />
-            <AiOutlineEdit
-              size={20}
-              className={style.icon}
-              color={colors.primary}
-            />
-          </div>
-        )}
-
-        <span>{year}</span>
-      </div>
-    </div>
+      {isUpdateModel && (
+        <ModelComponent
+          title='Update Education'
+          closeHandler={updateModelHandler}
+          validationSchema={validationSchema}
+          initalValues={educationInitalValue}
+          submitHandler={updateHandler}>
+          {() => {
+            return (
+              <>
+                <MyTextInput
+                  name='name'
+                  label='Name'
+                  placeholder='Enter Name'
+                />
+                <MyTextInput
+                  name='place'
+                  label='Place'
+                  placeholder='Enter Place'
+                />
+                <MyTextInput
+                  name='year'
+                  label='Year'
+                  placeholder='Enter Year'
+                />
+                <MyTextInput
+                  name='branch'
+                  label='Branch'
+                  placeholder='Enter Branch'
+                />
+              </>
+            );
+          }}
+        </ModelComponent>
+      )}
+    </>
   );
 }
--- src/screens/EducationScreen.jsx
+++ src/screens/EducationScreen.jsx
@@ -1,122 +1,130 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ScreenTemplete from '../templetes/ScreenTemplete';
 import { useSelector } from 'react-redux';
 import EducationDetailsCompoent from '../components/EducationDetailsCompoent';
-import ModelComponent from '../components/ModelComponent';
+import LoadingComponent from '../components/LoadingComponent';
 import MyTextInput from '../components/MyInputsCompoenent/MyTextInput';
 import * as yup from 'yup';
+const ModelComponent = lazy(() => import('../components/ModelComponent'));
 export default function EducationScreen() {
-  const [isEditModel, setEditModel] = React.useState(false);
-  const [isEducationModel, setEducationModel] = React.useState(false);
-  const editModalHandler = () => {
-    setEditModel((e) => !e);
-    return;
-  };
-  const educationModelHandler = () => {
-    setEducationModel((e) => !e);
-    return;
-  };
+  const [isPageEdit, setPageEdit] = React.useState(false);
+  const [isCreateModel, setCreateModel] = React.useState(false);
   const { title, courses } = useSelector((state) => state.admin.education);
   const { isAdmin } = useSelector((state) => state.admin);
   const colors = useSelector((state) => state.colors);
-  const initalValue = {
+  const pageValue = {
     title: title ? title : '',
   };
-  const validationSchema = yup.object({
-    title: yup.string().min(4).max(100).required(),
-  });
-  const submitHandler = (values) => {
-    console.log(values);
-  };
-  const educationInitalValue = {
+  const initalValue = {
     name: '',
     place: '',
     year: '',
     branch: '',
   };
-  const educationValidationSchema = yup.object({
+  const validationSchema = yup.object({
     name: yup.string().min(4).max(100).required(),
     place: yup.string().min(4).max(100).required(),
     year: yup.string().min(4).max(100).required(),
     branch: yup.string().min(4).max(100).required(),
   });
-  const educationSubmitHandler = (values) => {
+  const pageValidationSchema = yup.object({
+    title: yup.string().min(4).max(100).required(),
+  });
+  const pageModelToggler = () => {
+    setPageEdit((e) => !e);
+    return;
+  };
+  const createModelToggler = () => {
+    setCreateModel((e) => !e);
+    return;
+  };
+  const pageSubmitHandler = (values) => {
     console.log(values);
   };
+  const createHandler = (values) => {
+    console.log(values);
+  };
+  const updateHandler = () => {};
+  const deleteHandler = (id) => {};
   return (
-    <ScreenTemplete
-      title={title}
-      editHandler={editModalHandler}
-      isCreateButton
-      createHandler={educationModelHandler}>
-      {courses.map((course) => {
-        return (
-          <EducationDetailsCompoent
-            colors={colors}
-            isAdmin={isAdmin}
-            key={course.name}
-            name={course.name}
-            place={course.place}
-            year={course.year}
-            branch={course.branch}
-          />
-        );
-      })}
-      {isEditModel && (
-        <ModelComponent
-          title='Edit Page'
-          closeHandler={editModalHandler}
-          initalValues={initalValue}
-          validationSchema={validationSchema}
-          submitHandler={submitHandler}>
-          {() => {
-            return (
-              <>
-                <MyTextInput
-                  name='title'
-                  label='Title'
-                  placeholder='Enter Title'
-                />
-              </>
-            );
-          }}
-        </ModelComponent>
-      )}
-      {isEducationModel && (
-        <ModelComponent
-          title='Add Education'
-          closeHandler={educationModelHandler}
-          initalValues={educationInitalValue}
-          validationSchema={educationValidationSchema}
-          submitHandler={educationSubmitHandler}>
-          {() => {
-            return (
-              <>
-                <MyTextInput
-                  name='name'
-                  label='Name'
-                  placeholder='Enter Name'
-                />
-                <MyTextInput
-                  name='place'
-                  label='Place'
-                  placeholder='Enter Place'
-                />
-                <MyTextInput
-                  name='year'
-                  label='Year'
-                  placeholder='Enter Year'
-                />
-                <MyTextInput
-                  name='branch'
-                  label='Branch'
-                  placeholder='Enter Branch'
-                />
-              </>
-            );
-          }}
-        </ModelComponent>
-      )}
-    </ScreenTemplete>
+    <Suspense fallback={<LoadingComponent />}>
+      <ScreenTemplete
+        title={title}
+        editHandler={pageModelToggler}
+        isCreateButton
+        createHandler={createModelToggler}>
+        {courses.map((course) => {
+          return (
+            <EducationDetailsCompoent
+              colors={colors}
+              isAdmin={isAdmin}
+              key={course.name}
+              name={course.name}
+              place={course.place}
+              year={course.year}
+              branch={course.branch}
+              validationSchema={validationSchema}
+              updateHandler={updateHandler}
+              deleteHandler={deleteHandler}
+            />
+          );
+        })}
+        {isPageEdit && (
+          <ModelComponent
+            title='Edit Page'
+            closeHandler={pageModelToggler}
+            initalValues={pageValue}
+            validationSchema={pageValidationSchema}
+            submitHandler={pageSubmitHandler}>
+            {() => {
+              return (
+                <>
+                  <MyTextInput
+                    name='title'
+                    label='Title'
+                    placeholder='Enter Title'
+                  />
+                </>
+              );
+            }}
+          </ModelComponent>
+        )}
+        {isCreateModel && (
+          <ModelComponent
+            title='Add Education'
+            closeHandler={createModelToggler}
+            initalValues={initalValue}
+            validationSchema={validationSchema}
+            submitHandler={createHandler}>
+            {() => {
+              return (
+                <>
+                  <MyTextInput
+                    name='name'
+                    label='Name'
+                    placeholder='Enter Name'
+                  />
+                  <MyTextInput
+                    name='place'
+                    label='Place'
+                    placeholder='Enter Place'
+                  />
+                  <MyTextInput
+                    name='year'
+                    label='Year'
+                    placeholder='Enter Year'
+                  />
+                  <MyTextInput
+                    name='branch'
+                    label='Branch'
+                    placeholder='Enter Branch'
+                  />
+                </>
+              );
+            }}
+          </ModelComponent>
+        )}
+      </ScreenTemplete>
+    </Suspense>
   );
 }
