4c98d061aa2a7e231075605df3e589fbc129bdac
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-30 13:12:30 +0545

    added update features

--- src/components/TechnologyComponent.jsx
+++ src/components/TechnologyComponent.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import style from './style.module.scss';
 import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
-export default function TechnologyComponent({ name, icon, colors, isAdmin }) {
+import ImageAssets from '../assets/ImageAssets';
+import MyTextInput from './MyInputsCompoenent/MyTextInput';
+const ModelComponent = lazy(() => import('./ModelComponent'));
+export default function TechnologyComponent({
+  name,
+  icon,
+  colors,
+  isAdmin,
+  updateHandler,
+  validationSchema,
+  deleteHandler,
+}) {
+  const [isUpdateModel, setUpdateModel] = React.useState(false);
+  const updateModelToggler = () => {
+    setUpdateModel((e) => !e);
+  };
+  const initalValues = {
+    name,
+    icon,
+  };
   return (
     <div className={style.technologyContainer}>
       {isAdmin && (
@@ -9,10 +28,12 @@ export default function TechnologyComponent({ name, icon, colors, isAdmin }) {
           <AiOutlineDelete
             color={colors.warning}
             size={14}
+            onClick={deleteHandler}
             className={style.icon}
           />
           <AiOutlineEdit
             size={14}
+            onClick={updateModelToggler}
             className={style.icon}
             color={colors.primary}
           />
@@ -23,6 +44,32 @@ export default function TechnologyComponent({ name, icon, colors, isAdmin }) {
         <img src={icon} alt='icon' />
       </div>
       <span>{name}</span>
+      {isUpdateModel && (
+        <ModelComponent
+          title='Update Skills'
+          closeHandler={updateModelToggler}
+          initalValues={initalValues}
+          validationSchema={validationSchema}
+          submitHandler={updateHandler}>
+          {(value) => {
+            return (
+              <>
+                <ImageAssets path={value.icon} />
+                <MyTextInput
+                  name='icon'
+                  label='Icon'
+                  placeholder='Enter Icon'
+                />
+                <MyTextInput
+                  name='name'
+                  label='Name'
+                  placeholder='Enter Name'
+                />
+              </>
+            );
+          }}
+        </ModelComponent>
+      )}
     </div>
   );
 }
