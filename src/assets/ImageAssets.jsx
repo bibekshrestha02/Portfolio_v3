3ce0b7448a9da73c4708d5f6f9982d80a7cbefce
Bibek Shrestha <shresthabbks@gmail.com>
2021-06-30 10:33:11 +0545

    update image assets

--- src/assets/ImageAssets.jsx
+++ src/assets/ImageAssets.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './style.module.scss';
+import ImageLoadingComponent from '../components/ImageLoadingComponent';
 export default function ImageComponent({
-  image,
-  imageStyle,
+  path,
+  styles,
   containerStyle,
   imageAlt,
 }) {
+  const [isImageLoading, setSetImageloading] = useState(true);
+
   return (
     <div className={style.imageContainer} style={containerStyle}>
       <img
-        src={image}
-        alt={imageAlt}
-        style={imageStyle}
+        onLoad={() => setSetImageloading(false)}
+        onError={() => setSetImageloading(true)}
+        src={path}
+        alt={imageAlt ? imageAlt : 'image'}
+        style={{ display: isImageLoading ? 'none' : 'block', ...styles }}
         className={style.image}
       />
+      <ImageLoadingComponent isLoading={isImageLoading} />
     </div>
   );
 }
