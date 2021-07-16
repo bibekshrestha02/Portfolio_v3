04f49be0a81559b72263e444aaddae09689507f9
Bibek Shrestha <shresthabbks@gmail.com>
2021-07-16 12:22:19 +0545

    Created Login Screen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
-
+import FacebookLogin from 'react-facebook-login';
+import style from './style.module.scss';
+import { useDispatch } from 'react-redux';
+import { login } from '../store/actions/authActions';
+import { useHistory } from 'react-router-dom';
 export default function LoginScreen() {
-  return <div>Login Screen</div>;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const submitHandler = async (response) => {
+    try {
+      const { userID, accessToken } = response;
+      await dispatch(login(userID, accessToken));
+      history.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className={style.loginScreen}>
+      <FacebookLogin
+        appId='135247835406875'
+        autoLoad={false}
+        fields='name,email,picture'
+        callback={submitHandler}
+      />
+    </div>
+  );
 }
