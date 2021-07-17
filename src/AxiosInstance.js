ef10123c8759fdaa2f1fbab59756a0151efad644
Bibek Shrestha <shresthabbks@gmail.com>
2021-07-17 15:40:45 +0545

    Adding LocalStorage Token

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
-
+let token = localStorage.getItem('token');
 const instance = axios.create({
   baseURL: 'http://localhost:5000/api/v1',
   timeout: 2000,
+  headers: {
+    'x-auth-token': token ? token : '',
+  },
 });
 
 export default instance;
