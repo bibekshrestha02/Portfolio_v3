77648c4d7d3adabfed7841897b2ed0c85ebb68d4
Bibek Shrestha <shresthabbks@gmail.com>
2021-08-08 11:12:24 +0545

    Added Click Handler

--- src/assets/ButtonAssets.jsx
+++ src/assets/ButtonAssets.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import style from './style.module.scss';
 import { useSelector } from 'react-redux';
-export default function ButtonAssets({ props, title, styles, isSubmitting }) {
+export default function ButtonAssets({
+  props,
+  title,
+  styles,
+  isSubmitting,
+  clickHandler,
+}) {
   const { primary } = useSelector((state) => state.colors);
   return (
     <button
       style={{ backgroundColor: primary, ...styles }}
       className={style.button}
+      onClick={clickHandler}
       {...props}
       type='submit'>
       {isSubmitting ? <span>Please Wait...</span> : title}
